07f2675c15a9bad48fd55b59bbabba268ad466c8
Aiman Madan <64807286+AimanMadan@users.noreply.github.com>
2025-06-11 00:01:43 -0700

    init

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: process.env.NODE_ENV === 'production' ? '/user-org-dashboard' : '',
+  output: 'export',
 }
 
 export default nextConfig
